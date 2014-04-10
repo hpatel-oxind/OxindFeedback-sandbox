d79ea186a50374d430d538a13ab169b776283748
Hardik Patel <hpatel@oxind.com>
2014-04-10 16:26:42 +0530

    dashboard controller updated

--- src/Oxind/DashboardBundle/Controller/DefaultController.php
+++ src/Oxind/DashboardBundle/Controller/DefaultController.php
@@ -19,9 +19,16 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+        $request = $this->get('request');
         $feedbackTypeManager = $this->get('oxind_feedback.manager.feedbacktype');
-        $feedbackType = $feedbackTypeManager->findFeedbackTypeBy(array( 'name' => 'suggestion' ));
-        return $this->render('OxindDashboardBundle:Default:index.html.twig', array('feedbacktype' => $feedbackType));
+        $feedbackType = $feedbackTypeManager->findFeedbackTypeBy(array('name' => $feedbacktype_name));
+        if ($feedbackType == null)
+        {
+            throw $this->createNotFoundException();
+        }
+
+        $feedbackTypes = $feedbackTypeManager->findAllFeedBackType(array('suggestion', 'issue'));
+        return $this->render('OxindDashboardBundle:Default:index.html.twig', array('feedbacktype' => $feedbackType, 'feedbackTypes' => $feedbackTypes));
     }
 
 }
